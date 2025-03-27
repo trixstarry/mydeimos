9c27ba553caacedfa3a88b2d203a8b68cba6586c
Thao-Trang Hoang <5762115+trixstarry@users.noreply.github.com>
2025-03-26 21:40:38 -0700

    Update mydei.py to remove @

--- mydei.py
+++ mydei.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 df = px.data.iris()
 
-@st.experimental_memo
+st.experimental_memo
 def get_img_as_base64(file):
     with open(file, "rb") as f:
         data = f.read()
